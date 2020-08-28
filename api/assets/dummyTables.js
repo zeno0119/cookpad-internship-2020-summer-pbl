d09ea2868137aeeb5c5a71456af29a5ef0726666
zeno0119 <ishmael.ahab1851@gmail.com>
2020-08-28 15:10:05 +0900

    スタブを更新 ヘッダで食生活による検索を実装した

--- api/assets/dummyTables.js
+++ api/assets/dummyTables.js
@@ -16,5 +16,39 @@ module.exports.dummy = [
     recipe_id: 2,
     day: 1,
     when: 'dinner'
+  }, {
+    work_id: 1,
+    recipe_id: 0,
+    day: 1,
+    when: 'breakfast'
+  },
+  {
+    work_id: 2,
+    recipe_id: 1,
+    day: 1,
+    when: 'breakfast'
+  },
+  {
+    work_id: 2,
+    recipe_id: 3,
+    day: 1,
+    when: 'dinner'
+  }, {
+    work_id: 3,
+    recipe_id: 0,
+    day: 1,
+    when: 'breakfast'
+  },
+  {
+    work_id: 3,
+    recipe_id: 1,
+    day: 1,
+    when: 'lunch'
+  },
+  {
+    work_id: 3,
+    recipe_id: 2,
+    day: 1,
+    when: 'dinner'
   }
 ]
